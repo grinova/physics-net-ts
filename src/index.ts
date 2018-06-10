fe99e60e1797df5212526b6f61190494cf0963bf
Aleksandr Kuzmin <grinova@gmail.com>
2018-06-10 14:26:35 +0300

    Fixed main exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Message } from 'actors-ts'
 import { ControllerActor as _ControllerActor, ControllerActorProps as _ControllerActorProps } from './actors/controller-actor'
 import { Client as _Client } from './client'
 import { BaseController as _BaseController } from './controller/base-controller'
@@ -8,9 +9,13 @@ import { WebSocketNet as _WebSocketNet } from './net/websocket-net'
 
 export type ActorProps = _ActorProps
 export const Client = _Client
+export type Client = _Client
 export type Controller = _Controller
 export const ControllerActor = _ControllerActor
+export type ControllerActor<UserData, C extends Controller, M extends Message = Message> = _ControllerActor<UserData, C, M>
 export type ControllerActorProps<UserData, C extends Controller> = _ControllerActorProps<UserData, C>
 export const BaseController = _BaseController
+export type BaseController<UserData> = _BaseController<UserData>
 export type UserData = _UserData
 export const WebSocketNet = _WebSocketNet
+export type WebSocketNet = _WebSocketNet
